c0e3da2a07e5c9015cbcacc536ca63b8e06e9662
DUONGTHOAI <jupiter.at@gmail.com>
2016-11-08 23:31:37 +0700

    add home UI

---
+++ app/src/main/java/com/coderschool/booketplace/activities/LoginActivity.java
@@ -0,0 +1,19 @@
+package com.coderschool.booketplace.activities;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.coderschool.booketplace.BaseActivity;
+import com.coderschool.booketplace.R;
+
+/**
+ * Created by duongthoai on 11/8/16.
+ */
+
+public class LoginActivity extends BaseActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
---
+++ app/src/main/java/com/coderschool/booketplace/activities/SplashActivity.java
@@ -0,0 +1,47 @@
+package com.coderschool.booketplace.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.coderschool.booketplace.R;
+
+/**
+ * Created by duongthoai on 10/13/16.
+ */
+
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //
+                startHome();
+            }
+        }, 2000);
+    }
+
+    @Override
+    public void onBackPressed() {
+
+    }
+
+    private void startHome() {
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
+        //
+        finish();
+    }
+
+    private void startLogin() {
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
+        //
+        finish();
+    }
+}

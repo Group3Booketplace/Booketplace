d99147d7edaeea8a5a121d2dc866329b879ae04a
Tran Quang Dat <DatTran@Trans-MacBook-Pro-3.local>
2016-11-11 14:10:38 +0700

    Utils class

--- app/src/main/java/com/coderschool/booketplace/utils/FileUtils.java
+++ app/src/main/java/com/coderschool/booketplace/utils/FileUtils.java
@@ -1,8 +1,39 @@
 package com.coderschool.booketplace.utils;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.os.Environment;
+
+import com.coderschool.booketplace.R;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 /**
  * Created by DatTran on 11/11/16.
  */
 
 public class FileUtils {
+
+    public static File createPhotoFile(Context context) {
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_hhmmss", Locale.US).format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = new File(context.getExternalFilesDir(Environment.DIRECTORY_PICTURES), context.getString(R.string.app_name));
+        return new File(storageDir.getPath() + File.separator + imageFileName);
+    }
+
+    public static void store(Bitmap bitmap, String path) throws IOException {
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 40, bytes);
+        File resizeFile = new File(path);
+        resizeFile.createNewFile();
+        FileOutputStream fos = new FileOutputStream(resizeFile);
+        fos.write(bytes.toByteArray());
+        fos.close();
+    }
 }
--- app/src/main/java/com/coderschool/booketplace/utils/PermissionUtils.java
+++ app/src/main/java/com/coderschool/booketplace/utils/PermissionUtils.java
@@ -1,8 +1,46 @@
 package com.coderschool.booketplace.utils;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+
+import static android.os.Build.VERSION_CODES.M;
+
 /**
  * Created by DatTran on 11/11/16.
  */
 
 public class PermissionUtils {
+    public static final int REQUEST_LOCATION = 1000;
+    public static final int REQUEST_CAMERA = 1000;
+
+    public static void requestLocation(Activity activity) {
+        if (!isLocationGranted(activity)) {
+            ActivityCompat.requestPermissions(activity, new String[] {
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+            }, REQUEST_LOCATION);
+        }
+    }
+
+    public static void requestCamera(Activity activity) {
+        if (!isCameraGranted(activity)) {
+            ActivityCompat.requestPermissions(activity, new String[] {
+                    Manifest.permission.READ_EXTERNAL_STORAGE,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+            }, REQUEST_CAMERA);
+        }
+
+    }
+
+    public static boolean isLocationGranted(Activity activity) {
+        return ActivityCompat.checkSelfPermission(activity, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(activity, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED;
+    }
+
+    public static boolean isCameraGranted(Activity activity) {
+        return ActivityCompat.checkSelfPermission(activity, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED;
+    }
 }

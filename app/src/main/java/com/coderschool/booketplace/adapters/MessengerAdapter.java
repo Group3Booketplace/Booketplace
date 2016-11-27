d572c99ec2dbb59d692043ce264945185ba56962
ntvinh11586 <ntvinh.11586@gmail.com>
2016-11-27 18:00:21 +0700

    Update style on Messenger

--- app/src/main/java/com/coderschool/booketplace/adapters/MessengerAdapter.java
+++ app/src/main/java/com/coderschool/booketplace/adapters/MessengerAdapter.java
@@ -2,12 +2,16 @@ package com.coderschool.booketplace.adapters;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawable;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.target.BitmapImageViewTarget;
 import com.coderschool.booketplace.R;
 import com.coderschool.booketplace.activities.ChatActivity;
 import com.coderschool.booketplace.models.User;
@@ -48,9 +52,6 @@ public class MessengerAdapter extends RecyclerView.Adapter<RecyclerView.ViewHold
         final User user = mUsers.get(position);
 
         ((MessageViewHolder)viewHolder).getUsername().setText(user.getName());
-        Glide.with(mContext)
-                .load(user.getAvatar())
-                .into(((MessageViewHolder)viewHolder).getAvatar());
 
         ((MessageViewHolder)viewHolder).itemView
                 .setOnClickListener(new View.OnClickListener() {
@@ -59,6 +60,18 @@ public class MessengerAdapter extends RecyclerView.Adapter<RecyclerView.ViewHold
                         Intent intent = new Intent(getContext(), ChatActivity.class);
                         intent.putExtra(EXTRA_CHAT, user.getUid());
                         getContext().startActivity(intent);
+                    }
+                });
+        Glide.with(mContext)
+                .load(user.getAvatar())
+                .asBitmap().centerCrop()
+                .into(new BitmapImageViewTarget(((MessageViewHolder)viewHolder).getAvatar()) {
+                    @Override
+                    protected void setResource(Bitmap resource) {
+                        RoundedBitmapDrawable circularBitmapDrawable =
+                                RoundedBitmapDrawableFactory.create(mContext.getResources(), resource);
+                        circularBitmapDrawable.setCircular(true);
+                        ((MessageViewHolder)viewHolder).getAvatar().setImageDrawable(circularBitmapDrawable);
                     }
                 });
     }

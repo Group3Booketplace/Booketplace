37f6f6114b96d7cce4e64d656d2b7dabe28cef71
ntvinh11586 <ntvinh.11586@gmail.com>
2016-11-28 14:03:09 +0700

    Fix something errors

--- app/src/main/java/com/coderschool/booketplace/models/Messenger.java
+++ app/src/main/java/com/coderschool/booketplace/models/Messenger.java
@@ -1,5 +1,10 @@
 package com.coderschool.booketplace.models;
 
+import com.google.firebase.database.Exclude;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by vinh on 11/15/16.
  */
@@ -18,6 +23,16 @@ public class Messenger {
         this.uid = uid;
         this.date = date;
         this.lastMessage = lastMessage;
+    }
+
+    @Exclude
+    public Map<String, Object> toMap() {
+        HashMap<String, Object> result = new HashMap<>();
+        result.put("uid", uid);
+        result.put("lastMessage", lastMessage);
+        result.put("date", date);
+
+        return result;
     }
 
     public void setUser(User user) {
